04f49c0aadc4c86ec82d1a35c3378b1c5e8f5c2d
preverdy <preverdy@seas.upenn.edu>
2020-03-05 15:27:32 -0700

    More complete set of examples.

--- exampleCode.py
+++ exampleCode.py
@@ -15,3 +15,67 @@ n.simulate()
 n.mOut = n.parseMStates(n.zOut)
 
 n.mOptions = n.parseLeafStates(n.mOut)
+
+# Now, construct a tree with four options and explore the effect of v
+n = decisionNode() # root node 						0
+n.child0 = decisionNode() 				#	1				2
+n.child1 = decisionNode()		# options 1   2			  3   4
+
+# Default values of v result in deadlock.
+n.setup()
+n.simulate()
+
+n.mOut = n.parseMStates(n.zOut)
+n.mOptions = n.parseLeafStates(n.mOut)
+
+plot(n.tt, n.mOptions)
+
+# Increase v to destabilize deadlock
+n.child0.v *= 100
+n.child1.v *= 100
+
+n.setup()
+n.simulate()
+
+n.mOut = n.parseMStates(n.zOut)
+n.mOptions = n.parseLeafStates(n.mOut)
+
+plot(n.tt, n.mOptions)
+
+# Even with large v values symmetric initial conditions result in deadlock
+# Perturb initial conditions to avoid deadlock.
+n.m0[0] = 0.1
+n.child0.m0[1] = 0.1
+
+n.setup()
+n.simulate()
+
+n.mOut = n.parseMStates(n.zOut)
+n.mOptions = n.parseLeafStates(n.mOut)
+
+plot(n.tt, n.mOptions)
+
+# Perturb v to make a clear winner.
+n.child1.v[0] += 10
+n.child0.m0[0] = 0.1
+
+n.setup()
+n.simulate()
+
+n.mOut = n.parseMStates(n.zOut)
+n.mOptions = n.parseLeafStates(n.mOut)
+
+plot(n.tt, n.mOptions)
+
+# And now, perturb v again so there are two winners
+n.child1.v[1] += 10
+n.child0.m0[1] = 0.095 	# Make the initial condition symmetric
+
+n.setup()
+n.simulate()
+
+n.mOut = n.parseMStates(n.zOut)
+n.mOptions = n.parseLeafStates(n.mOut)
+
+figure()
+plot(n.tt, n.mOptions)
